4acd8688dbc85f394a343a048a9db6e74122c2e9
MatiasSanPietro <matisanpietro98@gmail.com>
2023-11-01 17:19:32 -0300

    entrega

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,15 +2,12 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const dbPort = process.env.DB_PORT || "3001";
-
 const sequelize = new Sequelize({
   dialect: "mysql",
   host: process.env.DB_HOST,
   database: process.env.DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  port: parseInt(dbPort, 10),
 });
 
 export default sequelize;
