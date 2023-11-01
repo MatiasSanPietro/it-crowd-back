26a99fac669f1989cc451894169fe236704471cc
MatiasSanPietro <matisanpietro98@gmail.com>
2023-11-01 16:08:03 -0300

    dbport

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -6,15 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+const dbPort = process.env.DB_PORT || "3001";
 const sequelize = new sequelize_1.Sequelize({
     dialect: "mysql",
     host: process.env.DB_HOST,
     database: process.env.DATABASE,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD || "root123",
+    port: parseInt(dbPort, 10),
 });
+exports.default = sequelize;
 // const sequelize = new Sequelize("itcrowd", "root", "root123", {
 //   host: "localhost",
 //   dialect: "mysql",
 // });
-exports.default = sequelize;
