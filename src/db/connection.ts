7493a641c7805553b39769a7f0ec647d279b5506
MatiasSanPietro <matisanpietro98@gmail.com>
2023-11-01 16:05:17 -0300

    dbport_fix

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,17 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const dbPort = process.env.DB_PORT || "3001";
+
 const sequelize = new Sequelize({
   dialect: "mysql",
   host: process.env.DB_HOST,
   database: process.env.DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD || "root123",
+  port: parseInt(dbPort, 10),
 });
+
+export default sequelize;
 
 // const sequelize = new Sequelize("itcrowd", "root", "root123", {
 //   host: "localhost",
 //   dialect: "mysql",
 // });
-
-export default sequelize;
